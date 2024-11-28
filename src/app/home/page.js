70bcb8abce618d926530f130dc4e47e2660eb633
yejiahn1 <yeji_09@naver.com>
2024-11-28 17:50:37 +0900

    chakra ui-Accordion

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,15 +1,17 @@
+import AccordionComponent from '@/components/accordion/Accordion';
 import { Header } from '@/components/layout/header/Header';
 import Section from '@/components/section/Section';
 import SlideSwiper from '@/components/swiper/Swiper';
+import { Accordion, Button } from '@chakra-ui/react';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <>
-            {' '}
             <Header />
             <SlideSwiper />
             <Section />/
+            <AccordionComponent />
         </>
     );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
-import localFont from 'next/font/local';
 import './globals.css';
+import { Providers } from '@/providers';
 
 export default function RootLayout({ children }) {
     return (
         <html lang="ko">
-            <body className=" bg-black h-[3000px]">{children}</body>
+            <body className="bg-black">
+                <Providers>{children}</Providers>
+            </body>
         </html>
     );
 }
